6b294d8aaca937a22e852aa8a6edd8fc3bb3e42c
Priyanshu Kumar <90020797+PRIYANSHU7763@users.noreply.github.com>
2022-10-11 12:18:17 +0530

    Added solution - LeetHub

---
+++ Anagram - GFG/anagram.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function is to check whether two strings are anagram of each other or not.
+    bool isAnagram(string a, string b){
+        
+        vector<int>fa(26,0);
+        vector<int>fb(26,0);
+        int na=a.size();
+        int nb=b.size();
+        if(na!=nb){
+            return 0;
+        }
+        for(int i=0;i<na;i++){
+            
+            fa[a[i]-'a']++;
+            fb[b[i]-'a']++;
+            }
+            
+            for(int i=0;i<26;i++){
+                if(fa[i]!=fb[i]){
+                    return 0;
+                }
+            }
+            return 1;
+        
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    
+    int t;
+
+    cin >> t;
+
+    while(t--){
+        string c, d;
+
+        cin >> c >> d;
+        Solution obj;
+        if(obj.isAnagram(c, d)) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+
+}
+
+// } Driver Code Ends
